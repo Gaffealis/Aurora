e0e3d12f7e34969fc2e90997e7f574bf792c81a4
theeldestelder <joshuag98@gmail.com>
2020-12-02 21:10:25 -0600

    Create new Avalonia MVVM project and add Visual Studio .gitignore

---
+++ src/Aurora/ViewModels/ViewModelBase.cs
@@ -0,0 +1,11 @@
+﻿using ReactiveUI;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Aurora.ViewModels
+{
+    public class ViewModelBase : ReactiveObject
+    {
+    }
+}
